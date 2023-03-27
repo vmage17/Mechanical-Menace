00885b9ac20f4d3f7faaeb20c63331fb0b792804
Radek <rjasiewicz@protonmail.com>
2023-03-27 21:16:26 +0200

    Implemented basic movement

--- src/Actors/Actor.cs
+++ src/Actors/Actor.cs
@@ -4,9 +4,11 @@ using System;
 public class Actor : KinematicBody2D
 {
 	
-	public Vector2 speed = new Vector2(300.0f, 1000.0f);
-	public float gravity = 1000.0f;
-	public Vector2 velocity = new Vector2(0.0f, 0.0f);
+	public static Vector2 FLOOR_NORMAL = new Vector2(0.0f, -1.0f);
+	protected const float gravity = 4000.0f;
+
+	protected Vector2 speed = new Vector2(500.0f, 1500.0f);
+	protected Vector2 velocity = new Vector2(0.0f, 0.0f);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -17,9 +19,8 @@ public class Actor : KinematicBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(float delta)
 	{
-		velocity.y += gravity * delta;
+		//GD.Print("_PhysicsProcess of Actor");
 		//if (velocity.y > speed.y)
 		//	velocity.y = speed.y;
-		velocity = MoveAndSlide(velocity);
 	}
 }
